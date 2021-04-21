ec0d0ecaaad1e77c10c5f61f73f41fd3148b16c5
Alisha Bolin <alishabolin@gmail.com>
2021-04-21 12:19:23 -0500

    Completed styling for Categories component. Also completed functionality forLetter component.

--- src/categories/Categories.js
+++ src/categories/Categories.js
@@ -1,3 +1,5 @@
+import './Categories.css';
+
 
 const Categories = () => {
     return(
@@ -8,6 +10,8 @@ const Categories = () => {
                     <h1>List 1</h1>
                 </div>
                 <ol className="Categories-Main-Display">
+                    <li>A Boy's Name</li>
+                    <li>A Boy's Name</li>
                     <li>A Boy's Name</li>
                 </ol>
             </div>
--- src/letter/Letter.js
+++ src/letter/Letter.js
@@ -1,14 +1,34 @@
 import './Letter.css';
+import { useState } from 'react';
 
 const Letter = () => {
+    // Using useState and destructuring to what it returns (array with two items [name, setName] = useState(starting value for name) - 'setName' is updated value for 'name')
+    const [index, setIndex] = useState(0);
+      
+    // 'letters' array
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    // Setting randomIndex function (inside changeLetter function), to choose a random letter from the letters array and set to value of 'setIndex' each time function is run - setIndex(randomIndex)
+    // set onClick on button (in return) to run changeLetter function upon each click
+    // set text inside h1 as the value of index as it corresponds to the letters array
+    const changeLetter = () => {
+        const randomIndex = Math.floor(Math.random() * letters.length);
+        setIndex(randomIndex);
+    }
+
     return(
         <div className="Letter">
             <div className="Letter-Top">
                 <p>LETTER</p>
-                <button className="Letter-Top-Btn">Roll Again</button>
+                <button
+                onClick={changeLetter}
+                className="Letter-Top-Btn"
+                >Roll Again</button>
             </div>
             <div className="Letter-Display">
-                <h1 className="Letter-Display-H1">A</h1>
+                <h1 className="Letter-Display-H1">
+                    {letters[index]}
+                </h1>
             </div>
         </div>
     );
